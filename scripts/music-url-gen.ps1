41db5e28e5ffd1c510bdee4e285069998c840ba8
Scott Yeung <yeunghl@whu.edu.cn>
2020-05-29 20:07:39 +0800

    switch script for gitee static rerouces

--- scripts/music-url-gen.ps1
+++ scripts/music-url-gen.ps1
@@ -1,8 +1,6 @@
-$new = ''
-
 foreach($f in (Get-ChildItem music -name))
 {
-    Write-Output "https://raw.githubusercontent.com/yeung66/resources/master/music/$f"
+    Write-Output "https://gitee.com/yeungyeah/resources/raw/master/music/$f"
 }
 
 
